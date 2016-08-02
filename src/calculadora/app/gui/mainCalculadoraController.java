62b17f15899a2d0f99dcafabc3466d5432d9acdf
Thiago <Thiago@Thiago-PC>
2016-08-02 08:57:37 -0300

    Adicionadas as funções de soma, subtração, multiplicação e divisão. Agora todos os botões possuem uma função e a label de resposta apresenta o resultado conforme solicitado. Devo verificar a forma correta de setar a variável isFloat para que um valor posterior não herde a característica do valor anterior.

--- src/calculadora/app/gui/mainCalculadoraController.java
+++ src/calculadora/app/gui/mainCalculadoraController.java
@@ -15,112 +15,246 @@ import javafx.scene.control.Label;
 
 public class mainCalculadoraController implements Initializable {
     
-    protected int MAX_DIGITS = 13;
+    protected int MAX_DIGITS = 12, operation;
     
-    private Button appButton = new Button();
+    private final Button appButton = new Button();
     
-    private String firstNumber, secondNumber;
+    private double firstNumber;
     
-    private boolean isFloat;
+    private boolean isFloat, mustClean;
     
     @FXML
     private Label appLabel;
     
     @FXML
     private void handleButtonEnter(ActionEvent event){
-        System.out.println("LABEL =>" + appLabel.getText());
-        int n = Integer.parseInt(appLabel.getText());
-        System.out.println("INT NUMBER =>" + n);
+        
+        double total = 0;
+        
+        switch(operation){
+            case 0:
+                total = firstNumber + Double.parseDouble(appLabel.getText());
+                break;
+            case 1:
+                total = firstNumber - Double.parseDouble(appLabel.getText());
+                break;
+            case 2:
+                total = firstNumber * Double.parseDouble(appLabel.getText());
+                break;
+            case 3:
+                total = firstNumber / Double.parseDouble(appLabel.getText());
+                break;
+        }
+        
+        if (isFloat)
+            appLabel.setText(Double.toString(total));
+        else 
+            appLabel.setText(Integer.toString((int) total));
+        mustClean = true;
+ 
     }
     
     @FXML
     private void handleButtonComa(ActionEvent event){
-        System.out.println("calculadora.app.gui.mainCalculadoraController.handleButtonComa()");
+        if(mustClean) {
+            appLabel.setText("0");
+            mustClean = false;
+            isFloat = false;
+        }
+        if (appLabel.getText().length() < MAX_DIGITS){
+            isFloat = true;
+            appLabel.setText(appLabel.getText() + ".");
+        }
     }
     
     @FXML
     private void handleButtonZero(ActionEvent event){
-        if (appLabel.getText().length() < MAX_DIGITS)
-            appLabel.setText(Integer.toString(Integer.parseInt(appLabel.getText() + "0")));
+        if(mustClean) {
+            appLabel.setText("0");
+            mustClean = false;
+        }
+        if (appLabel.getText().length() < MAX_DIGITS)            
+            if (!isFloat) {
+                    appLabel.setText(Long.toString(Long.parseLong(appLabel.getText() + "0")));
+            } 
+            else {
+                    appLabel.setText(appLabel.getText() + "0");
+            }
     }
     
     @FXML
     private void handleButtonOne(ActionEvent event){
+        if(mustClean) {
+            appLabel.setText("0");
+            mustClean = false;
+        }
         if (appLabel.getText().length() < MAX_DIGITS)
-            appLabel.setText(Integer.toString(Integer.parseInt(appLabel.getText() + "1"))); 
+            if (!isFloat) {
+                    appLabel.setText(Long.toString(Long.parseLong(appLabel.getText() + "1"))); 
+                } 
+            else {
+                    appLabel.setText(Double.toString(Double.parseDouble(appLabel.getText() + "1")));
+            }
     }
     
     @FXML
     private void handleButtonTwo(ActionEvent event){
-            appLabel.setText(Integer.toString(Integer.parseInt(appLabel.getText() + "2")));
+        if(mustClean) {
+            appLabel.setText("0");
+            mustClean = false;
+        }
+        if (appLabel.getText().length() < MAX_DIGITS)
+            if (!isFloat) {
+                    appLabel.setText(Long.toString(Long.parseLong(appLabel.getText() + "2"))); 
+                } 
+            else {
+                    appLabel.setText(Double.toString(Double.parseDouble(appLabel.getText() + "2")));
+            }
     }
     
     @FXML
     private void handleButtonThree(ActionEvent event){
+        if(mustClean) {
+            appLabel.setText("0");
+            mustClean = false;
+        }
         if (appLabel.getText().length() < MAX_DIGITS)
-            appLabel.setText(Integer.toString(Integer.parseInt(appLabel.getText() + "3")));
+            if (!isFloat) {
+                    appLabel.setText(Long.toString(Long.parseLong(appLabel.getText() + "3"))); 
+                } 
+            else {
+                    appLabel.setText(Double.toString(Double.parseDouble(appLabel.getText() + "3")));
+            }
     }
     
     @FXML
     private void handleButtonFour(ActionEvent event){
-        if (appLabel.getText().length() < MAX_DIGITS)    
-            appLabel.setText(Integer.toString(Integer.parseInt(appLabel.getText() + "4")));
+        if(mustClean) {
+            appLabel.setText("0");
+            mustClean = false;
+        }
+        if (appLabel.getText().length() < MAX_DIGITS)
+            if (!isFloat) {
+                    appLabel.setText(Long.toString(Long.parseLong(appLabel.getText() + "4"))); 
+                } 
+            else {
+                    appLabel.setText(Double.toString(Double.parseDouble(appLabel.getText() + "4")));
+            }
     }
     
     @FXML
     private void handleButtonFive(ActionEvent event){
-        if (appLabel.getText().length() < MAX_DIGITS)   
-            appLabel.setText(Integer.toString(Integer.parseInt(appLabel.getText() + "5")));
+        if(mustClean) {
+            appLabel.setText("0");
+            mustClean = false;
+        }
+        if (appLabel.getText().length() < MAX_DIGITS)
+            if (!isFloat) {
+                    appLabel.setText(Long.toString(Long.parseLong(appLabel.getText() + "5"))); 
+                } 
+            else {
+                    appLabel.setText(Double.toString(Double.parseDouble(appLabel.getText() + "5")));
+            }
     }
     
     @FXML
     private void handleButtonSix(ActionEvent event){
+        if(mustClean) {
+            appLabel.setText("0");
+            mustClean = false;
+        }
         if (appLabel.getText().length() < MAX_DIGITS)
-            appLabel.setText(Integer.toString(Integer.parseInt(appLabel.getText() + "6")));
+            if (!isFloat) {
+                    appLabel.setText(Long.toString(Long.parseLong(appLabel.getText() + "6"))); 
+                } 
+            else {
+                    appLabel.setText(Double.toString(Double.parseDouble(appLabel.getText() + "6")));
+            }
     }
     
     @FXML
     private void handleButtonSeven(ActionEvent event){
+        if(mustClean) {
+            appLabel.setText("0");
+            mustClean = false;
+        }
         if (appLabel.getText().length() < MAX_DIGITS)
-            appLabel.setText(Integer.toString(Integer.parseInt(appLabel.getText() + "7")));
+            if (!isFloat) {
+                    appLabel.setText(Long.toString(Long.parseLong(appLabel.getText() + "7"))); 
+                } 
+            else {
+                    appLabel.setText(Double.toString(Double.parseDouble(appLabel.getText() + "7")));
+            }
     }
     
     @FXML
     private void handleButtonEight(ActionEvent event){
+        if(mustClean) {
+            appLabel.setText("0");
+            mustClean = false;
+        }
         if (appLabel.getText().length() < MAX_DIGITS)
-            appLabel.setText(Integer.toString(Integer.parseInt(appLabel.getText() + "8")));
+            if (!isFloat) {
+                    appLabel.setText(Long.toString(Long.parseLong(appLabel.getText() + "8"))); 
+                } 
+            else {
+                    appLabel.setText(Double.toString(Double.parseDouble(appLabel.getText() + "8")));
+            }
     }
     
     @FXML
     private void handleButtonNine(ActionEvent event){
+        if(mustClean) {
+            appLabel.setText("0");
+            mustClean = false;
+        }
         if (appLabel.getText().length() < MAX_DIGITS)
-            appLabel.setText(Integer.toString(Integer.parseInt(appLabel.getText() + "9")));
+            if (!isFloat) {
+                    appLabel.setText(Long.toString(Long.parseLong(appLabel.getText() + "9"))); 
+                } 
+            else {
+                    appLabel.setText(Double.toString(Double.parseDouble(appLabel.getText() + "9")));
+            }
     }
     
     @FXML
     private void handleButtonPlus(ActionEvent event){
-        System.out.println("calculadora.app.gui.mainCalculadoraController.handleButtonThree()");
+        firstNumber = Double.parseDouble(appLabel.getText());
+        mustClean = true;
+        isFloat = false;
+        operation = 0;
     }
     
     @FXML
     private void handleButtonMinus(ActionEvent event){
-        System.out.println("calculadora.app.gui.mainCalculadoraController.handleButtonThree()");
+        firstNumber = Double.parseDouble(appLabel.getText());
+        mustClean = true;
+        isFloat = false; 
+        operation = 1;
     }
     
         @FXML
     private void handleButtonTimes(ActionEvent event){
-        System.out.println("calculadora.app.gui.mainCalculadoraController.handleButtonThree()");
+        firstNumber = Double.parseDouble(appLabel.getText());
+        mustClean = true;
+        isFloat = false;  
+        operation = 2;
     }
     
         @FXML
     private void handleButtonDivided(ActionEvent event){
-        System.out.println("calculadora.app.gui.mainCalculadoraController.handleButtonThree()");
+        firstNumber = Double.parseDouble(appLabel.getText());
+        mustClean = true;
+        isFloat = false;
+        operation = 3;
     }
     
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         
-        appLabel.setText("");
+        appLabel.setText("0");
+        isFloat = false;
+        mustClean = false;
         appButton.setOnAction((event) -> this.handleButtonEnter(event));
         appButton.setOnAction((event) -> this.handleButtonComa(event));
         appButton.setOnAction((event) -> this.handleButtonZero(event));
